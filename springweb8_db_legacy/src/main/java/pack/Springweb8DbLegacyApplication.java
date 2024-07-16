14a907bbb4823766a00308a295aa8985c55112ff
yeongenn <yngn0012@naver.com>
2024-07-16 13:21:59 +0900

    240716 Spring Boot DB access (1) - legacy

---
+++ springweb8_db_legacy/src/main/java/pack/Springweb8DbLegacyApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb8DbLegacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springweb8DbLegacyApplication.class, args);
+	}
+
+}
