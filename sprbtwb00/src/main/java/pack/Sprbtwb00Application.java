adbb1bcdfd06f887514c5296ce73879a8738964d
yeongenn <yngn0012@naver.com>
2024-07-16 13:01:58 +0900

    240716 Spring Boot Gradle

---
+++ sprbtwb00/src/main/java/pack/Sprbtwb00Application.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprbtwb00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprbtwb00Application.class, args);
+	}
+
+}
