fb2ee284213db3b7aed8ac8a349810157aca1ae9
yeongenn <yngn0012@naver.com>
2024-07-16 15:24:46 +0900

    240716 Spring Boot DB access (1) 문제풀이

---
+++ springweb8_db_legacy_ex/src/main/java/pack/Springweb8DbLegacyExApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb8DbLegacyExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springweb8DbLegacyExApplication.class, args);
+	}
+
+}
