2fd0402fcc5f6db1f97bc34d8c7a54041d51decb
yeongenn <yngn0012@naver.com>
2024-07-19 12:11:04 +0900

    240718 Spring Boot DB access (4) JPA intro

--- springweb13_db_jpa_basic/src/main/java/pack/other/ProductCrudRepository.java
+++ springweb13_db_jpa_basic/src/main/java/pack/other/ProductCrudRepository.java
@@ -3,7 +3,7 @@ package pack.other;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductCrudRepository extends CrudRepository<ProductVo, Integer>{
-	// CrudRepository<T, ID> - T(Entity), ID(PK 타입)
+	// CrudRepository<T, ID> - T(Entity), ID(PK의 타입)
 	// save(), findById(), count(), ... 기본적으로 지원 받음
 	// 추가하고 싶은 메서드가 있다면 작성해주면 된다
 	
