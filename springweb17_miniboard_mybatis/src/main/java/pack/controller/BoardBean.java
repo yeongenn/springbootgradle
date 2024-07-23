f193171814c347c734b5bf0496a776ec75a30b60
yeongenn <yngn0012@naver.com>
2024-07-23 18:54:12 +0900

    240723 binding requests to controller

--- springweb17_miniboard_mybatis/src/main/java/pack/controller/BoardBean.java
+++ springweb17_miniboard_mybatis/src/main/java/pack/controller/BoardBean.java
@@ -7,7 +7,14 @@ import lombok.Setter;
 @Setter
 
 public class BoardBean {
-    private int num, readcnt;
-    private String author, title, content, bwrite;
+    private int num;
+    private String title, content, bwrite;
     private String searchName, searchValue;
+    private int readcnt;    
+    private String author;
+    
+//    // 필드명이랑 다르면 요청값이 바인딩 되지 X
+//    private String writer;
+    
+    
 }
