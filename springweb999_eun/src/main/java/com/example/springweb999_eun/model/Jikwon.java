c2e689426b2d161c26dcfcd773f43702edf67e00
yeongenn <yngn0012@naver.com>
2024-08-07 15:49:13 +0900

    240807 JoinColumn 설명 주석 추가

--- springweb999_eun/src/main/java/com/example/springweb999_eun/model/Jikwon.java
+++ springweb999_eun/src/main/java/com/example/springweb999_eun/model/Jikwon.java
@@ -30,6 +30,15 @@ public class Jikwon {
     @ManyToOne//(fetch = FetchType.EAGER) // 테이블 연관관계
     @JoinColumn(name="buser_num", referencedColumnName = "buser_no")
     private Buser buser;
+    
+    /*
+     @JoinColumn
+     name : 대상 엔티티와 매핑할 외래키(자기 필드명) 이름을 지정
+     referencedColumnName : 조인할 칼럼명(외래키가 참조하는 대상 테이블의 칼럼명)을 지정
+     
+     근데 referencedColumnName는 적어주지 않아도 알아서 맵핑이 되는데?
+     referencedColumnName의 default 값이 참조하는 대상 테이블의 기본키의 변수명으로 알아서 조인하도록 동작되거든
+     */
 
     @OneToMany(mappedBy = "jikwon")
     private List<Gogek> gogekList;
