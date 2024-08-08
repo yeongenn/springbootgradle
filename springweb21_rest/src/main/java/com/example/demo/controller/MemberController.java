9373359495d58fa1588d9859d9ba87075d9c77ff
yeongenn <yngn0012@naver.com>
2024-08-08 13:15:26 +0900

    240808 REST - CRUD (vue.js)

--- springweb21_rest/src/main/java/com/example/demo/controller/MemberController.java
+++ springweb21_rest/src/main/java/com/example/demo/controller/MemberController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.MemberDto;
