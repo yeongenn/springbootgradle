878fba5299309ab90e569c63f0959c4d5ecfc062
yeongenn <yngn0012@naver.com>
2024-08-26 16:53:04 +0900

    240826 login (1) - spring security 적용 전, session으로

---
+++ springweb26_former_security/src/main/java/pack/Springweb26FormerSecurityApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb26FormerSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springweb26FormerSecurityApplication.class, args);
+	}
+
+}
