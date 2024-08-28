d4304b05b580ff46fc2555277f9ff48d187e1031
yeongenn <yngn0012@naver.com>
2024-08-28 12:53:56 +0900

    240828 spring security - db 연동

---
+++ springweb29_security_db/src/main/java/pack/Springweb29SecurityDbApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb29SecurityDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springweb29SecurityDbApplication.class, args);
+	}
+
+}
