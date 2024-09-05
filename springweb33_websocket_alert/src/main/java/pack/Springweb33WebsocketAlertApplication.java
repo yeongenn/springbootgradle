d27508816a8d242bf3a7135b0995734af2cb2d63
yeongenn <yngn0012@naver.com>
2024-09-05 19:05:09 +0900

    240905 spring boot - websocket

---
+++ springweb33_websocket_alert/src/main/java/pack/Springweb33WebsocketAlertApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb33WebsocketAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springweb33WebsocketAlertApplication.class, args);
+	}
+
+}
